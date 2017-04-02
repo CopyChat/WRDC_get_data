b02316c8dbba46ef0a2681ba2b130921e4f4f351
ctang <ctang@macbook-pro-de-ctang-1.home>
2017-04-02 10:28:03 +0400

    add indicatoer use command line liks to download, get info of year and month, simplify the check function.

--- code/get.WRDC.southern.africa.sh
+++ code/get.WRDC.southern.africa.sh
@@ -11,7 +11,7 @@ USAGE="./get.WRDC.southern.africa.sh  "
 #  REQUIREMENTS: ---
 #                   1. link.country_station, checked by eyes !!!
 #                   2. get.WRDC.data.py 
-#          BUGS: --- no process indicators
+#          BUGS: --- no process indicators (fixed)
 #         NOTES: ---
 #        AUTHOR: Tang (Tang), chao.tang.1@gmail.com
 #  ORGANIZATION: le2p
@@ -36,58 +36,77 @@ function get_link()
     do
         l1=$(awk 'NR=='$i'' $link)
         echo $i,$l1
-        curl --silent $l1 | grep data_list_full_protected | awk -F "\"" '{print $2}' >> link.country_station_year.temp
+        curl --silent $l1 | grep data_list_full_protected | awk -F "\"" '{print $2}' >> link.country_station_year
     done
-    awk '{ gsub(".html", ".1.csv.html") ; print $0 }' link.country_station_year.temp > link.country_station_year
 }
 
 function get_info()
 {
-
     file='link.country_station_year'
     for a in $(cat $file); do b=${a##*t1/}; c=${b%%/*};  echo $c ; done > link.country
     for a in $(cat link.country_station_year); do b=${a##*/}; c=${b%_t1*}; d=${c%*_????}; echo $d; done > link.station
     for a in $(cat $file); do b=${a##*/}; c=${b%_*}; d=${c##*_}; echo $d; done > link.year
 }
 
+function getdata()
+{
+    link='link.country_station_year'
+    line=$(wc -l < $link)
 
 
+    for i in $(seq -s " " 1 $line)
+    do
+        l1=$(awk 'NR=='$i'' $link)
+
+        country=$(awk 'NR=='$i'' link.country)
+        station=$(awk 'NR=='$i'' link.station)
+        year=$(awk 'NR=='$i'' link.year)
+
+        # download:
+        output=WRDC.$country.$station.$year.line_$i.csv
+        links -dump $l1 > $output
+
+        echo "============= $i/$line"
+    done
+}
 
 #=================================================== 
-# to check is all the data are well downloaded:
+# to check is all the data are well downloaded: 94 lines each file
 function check()
 {
-line=$(wc -l < link.country_station_year)
+    line=$(wc -l < link.country_station_year)
 
-# if not, output the failed iterms to :
-output=redo.temp
+    # if not, output the failed iterms to :
+    output=redo.temp
 
-for i in $(seq -s " " 1 $line)
-do
-    wc=$(wc -l WRDC.*.*.*.line?$i.csv | awk '{print $1}')
-    echo $i,$wc
-    if ! [ $wc -eq 41 ];
-    then
+    for i in $(seq -s " " 1 $line)
+    do
+        wc=$(wc -l WRDC.*.*.*.line?$i.csv | awk '{print $1}')
         echo $i,$wc
-        awk 'NR=='$i'' link.station.southern.africa >> $output
+        if ! [ $wc -eq 41 ];
+        then
+            echo $i,$wc
+            awk 'NR=='$i'' link.station.southern.africa >> $output
 
-        awk 'NR=='$i'' link.station >> $output.station
-        awk 'NR=='$i'' link.country >> $output.country
-        awk 'NR=='$i'' link.year >> $output.year
-    fi
-done
+            awk 'NR=='$i'' link.station >> $output.station
+            awk 'NR=='$i'' link.country >> $output.country
+            awk 'NR=='$i'' link.year >> $output.year
+        fi
+    done
 
 }
 
 #get_link 
+
+# start to download
 #get_info
 
 # download using get.WRDC.data.py
 # NOTE: output file name : line number is the line of link.country_station_year
 #python ./get.WRDC.data.py >> download.log
 
+getdata
 
 # check if all are downloaded
-check
-
-# change get.WRDC.data.py to download these.
+#check
+# change the value of 'link' variable & rerun getdata to download these "missing" data.
